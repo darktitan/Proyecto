3044caf4a398bc0eaf2efdde0c331ef6b5cf6c5a
Orlando Giron <orlandogironb@gmail.com>
2019-02-21 19:53:13 -0600

    cambios
    estos cambios se realia ddddd

---
+++ test - copia - copia/program1.cs
@@ -0,0 +1,11 @@
+class Programa1
+{
+public static void Main(string []args)
+{
+int edad=22;
+double est=55.9;
+string info="Gisela";
+System.Console.WriteLine("HOLA"+info+"Su edad es"+(edad+est));
+}
+
+}
---
+++ test - copia - copia/whiler_ejemplo1.cs
@@ -0,0 +1,14 @@
+using System;
+class EstructuraFor
+{
+public static void Main()
+{
+int i=0,n;
+n=int.Parse(Console.ReadLine());
+while(i<=n)
+{
+Console.WriteLine(i);
+i=i+1;
+}
+}
+}
